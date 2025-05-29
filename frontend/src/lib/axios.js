dbb295c241324276a84a497d0337750002008bc5
LAPTOP-GBK77T8O\Asus <levietnguyenhung0711@gmail.com>
2025-05-30 02:35:42 +0700

    done build docker for fe web

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,9 @@
-// Thêm interceptor vào file axios.js để tự động gắn token vào mỗi request
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: import.meta.env.PROD 
+    ? "/api"  // Production (Docker)
+    : "http://localhost:3000/api", // Development
   withCredentials: true,
   headers: {
     "x-client-type": "web",
@@ -16,12 +17,12 @@ axiosInstance.interceptors.request.use(
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
-    
+
     // Chỉ thiết lập Content-Type: application/json khi không phải FormData
     if (!(config.data instanceof FormData)) {
       config.headers["Content-Type"] = "application/json";
     }
-    
+
     return config;
   },
   (error) => Promise.reject(error)
